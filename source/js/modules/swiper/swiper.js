36125cee53b47a248daf040316caba1f738d5dcb
Alina <alina.melikhova@gmail.com>
2023-07-08 10:20:57 +0300

    does smthng

---
+++ source/js/modules/swiper/swiper.js
@@ -0,0 +1,16 @@
+// import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs';
+// import 'swiper/css/bundle';
+
+console.log('alino44ka');
+// const swiper = new Swiper();
+
+// export const swiper = new Swiper('.swiper', {
+//   direction: 'horizontal',
+//   loop: true,
+//   slidesPerView: 1,
+
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// });
